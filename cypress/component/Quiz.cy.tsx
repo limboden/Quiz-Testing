05b150008ba45c47716ae303c51c4b6b9b877bf7
limboden <imbodenlu@gmail.com>
2024-12-04 18:33:07 -0500

    this test works aswell, but its all about the timing of the first click. the start test button lags so much for some reason

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -3,27 +3,37 @@ import Quiz from '../../client/src/components/Quiz';
 import React from 'react';
 
 describe('Quiz', () => {
-  const waitTime = 25000
+  const waitTime = 5000
 
   beforeEach(() => {
     mount(<Quiz />);
   });
 
-  it('starts the quiz', () => {
-    cy.contains("Start Quiz").click();
-    cy.wait(waitTime)
-    cy.get(".card").should("exist");
-  })
+  // it('starts the quiz', () => {
+  //   cy.contains("Start Quiz").click();
+  //   cy.wait(waitTime)
+  //   cy.get(".card").should("exist");
+  // })
 
-  it("shows the next question after answering", () => {
-    cy.contains("Start Quiz").click();
-    cy.wait(waitTime)
-    cy.get(".btn-primary").first().click();
-    cy.wait(waitTime)
-    cy.get(".card h2").should("exist");
-  });
+  // it("shows the next question after answering", () => {
+  //   cy.contains("Start Quiz").click();
+  //   cy.wait(waitTime)
+  //   cy.get(".btn-primary").first().click();
+  //   cy.wait(waitTime)
+  //   cy.get(".card h2").should("exist");
+  // });
 
-  it("ends the quiz after all questions are answered", () => {
+  // it("ends the quiz after all questions are answered", () => {
+  //   cy.contains("Start Quiz").click();
+  //   cy.wait(waitTime)
+  //   for (let i = 0; i < 10; i++) {
+  //     cy.get(".btn-primary").contains("1").click();
+  //     cy.wait(waitTime)
+  //   }
+  //   cy.contains("Quiz Completed").should("be.visible");
+  // });
+
+  it("displays the score after completion", () => {
     cy.contains("Start Quiz").click();
     cy.wait(waitTime)
     for (let i = 0; i < 10; i++) {
@@ -31,5 +41,6 @@ describe('Quiz', () => {
       cy.wait(waitTime)
     }
     cy.contains("Quiz Completed").should("be.visible");
+    cy.get(".alert.alert-success").should("contain", "Your score:");
   });
 })
