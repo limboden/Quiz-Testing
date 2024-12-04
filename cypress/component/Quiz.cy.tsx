c679ac998d734c7e86b82a51bbfc3e53c37509e2
limboden <imbodenlu@gmail.com>
2024-12-04 18:05:53 -0500

    it passes the test! (sometimes?)

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -3,6 +3,7 @@ import Quiz from '../../client/src/components/Quiz';
 import React from 'react';
 
 describe('Quiz', () => {
+  const waitTime = 5000
 
   beforeEach(() => {
     mount(<Quiz />);
@@ -11,6 +12,7 @@ describe('Quiz', () => {
 
   it('starts the quiz', () => {
     cy.contains("Start Quiz").click();
+    cy.wait(waitTime)
     cy.get(".card").should("exist");
   })
 })
