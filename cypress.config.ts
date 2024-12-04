b7d6ad608221cb9b86816ac1d3d88d019bcfdefb
limboden <imbodenlu@gmail.com>
2024-12-04 17:57:46 -0500

    i think i set this up correctly...

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -1,5 +1,16 @@
-describe('Quiz.cy.tsx', () => {
-  it('playground', () => {
-    // cy.mount()
+import { mount } from 'cypress/react'
+import Quiz from '../../client/src/components/Quiz';
+import React from 'react';
+
+describe('Quiz', () => {
+
+  beforeEach(() => {
+    mount(<Quiz />);
+  });
+
+
+  it('starts the quiz', () => {
+    cy.contains("Start Quiz").click();
+    cy.get(".card").should("exist");
   })
 })
