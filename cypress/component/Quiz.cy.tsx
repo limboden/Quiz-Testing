a6128bc3978b793a5c252a78a0020ea07bb09be6
limboden <imbodenlu@gmail.com>
2024-12-04 18:43:14 -0500

    uncommented all the other tests

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -4,57 +4,58 @@ import React from 'react';
 
 describe('Quiz', () => {
   const waitTime = 2000
+  const longWaitTime = 40000
 
   beforeEach(() => {
     mount(<Quiz />);
   });
 
-  // it('starts the quiz', () => {
-  //   cy.contains("Start Quiz").click();
-  //   cy.wait(waitTime)
-  //   cy.get(".card").should("exist");
-  // })
+  it('starts the quiz', () => {
+    cy.contains("Start Quiz").click();
+    cy.wait(longWaitTime)
+    cy.get(".card").should("exist");
+  })
 
-  // it("shows the next question after answering", () => {
-  //   cy.contains("Start Quiz").click();
-  //   cy.wait(waitTime)
-  //   cy.get(".btn-primary").first().click();
-  //   cy.wait(waitTime)
-  //   cy.get(".card h2").should("exist");
-  // });
+  it("shows the next question after answering", () => {
+    cy.contains("Start Quiz").click();
+    cy.wait(longWaitTime)
+    cy.get(".btn-primary").first().click();
+    cy.wait(waitTime)
+    cy.get(".card h2").should("exist");
+  });
 
-  // it("ends the quiz after all questions are answered", () => {
-  //   cy.contains("Start Quiz").click();
-  //   cy.wait(waitTime)
-  //   for (let i = 0; i < 10; i++) {
-  //     cy.get(".btn-primary").contains("1").click();
-  //     cy.wait(waitTime)
-  //   }
-  //   cy.contains("Quiz Completed").should("be.visible");
-  // });
+  it("ends the quiz after all questions are answered", () => {
+    cy.contains("Start Quiz").click();
+    cy.wait(longWaitTime)
+    for (let i = 0; i < 10; i++) {
+      cy.get(".btn-primary").contains("1").click();
+      cy.wait(waitTime)
+    }
+    cy.contains("Quiz Completed").should("be.visible");
+  });
 
-  // it("displays the score after completion", () => {
-  //   cy.contains("Start Quiz").click();
-  //   cy.wait(waitTime)
-  //   for (let i = 0; i < 10; i++) {
-  //     cy.get(".btn-primary").contains("1").click();
-  //     cy.wait(waitTime)
-  //   }
-  //   cy.contains("Quiz Completed").should("be.visible");
-  //   cy.get(".alert.alert-success").should("contain", "Your score:");
-  // });
+  it("displays the score after completion", () => {
+    cy.contains("Start Quiz").click();
+    cy.wait(longWaitTime)
+    for (let i = 0; i < 10; i++) {
+      cy.get(".btn-primary").contains("1").click();
+      cy.wait(waitTime)
+    }
+    cy.contains("Quiz Completed").should("be.visible");
+    cy.get(".alert.alert-success").should("contain", "Your score:");
+  });
 
 
   it("allows starting a new quiz", () => {
     cy.contains("Start Quiz").click();
-    cy.wait(30000)
+    cy.wait(longWaitTime)
     for (let i = 0; i < 10; i++) {
       cy.get(".btn-primary").contains("1").click();
       cy.wait(waitTime)
     }
     cy.contains("Quiz Completed").should("be.visible");
     cy.contains("Take New Quiz").click();
-    cy.wait(30000)
+    cy.wait(longWaitTime)
     cy.get(".btn-primary").contains("1").should("be.visible");
   });
 })
