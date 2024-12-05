b8c6e2d9f48f19bb4b97aa40b5668b59eaecd6db
limboden <imbodenlu@gmail.com>
2024-12-04 19:18:44 -0500

    between each test, revisit the main page

--- cypress/e2e/Quiz.cy.ts
+++ cypress/e2e/Quiz.cy.ts
@@ -2,4 +2,10 @@ describe('Quiz site', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  beforeEach(() => {
+    cy.visit("http://localhost:3002/");
+  });
+
+
 })
